25c2ef41d93b3e58f2338e01a19b522ad0797425
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:03:28 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "sentinel_alert_rule_anomaly_duplicates" {
+  description = <<EOT
+Map of sentinel_alert_rule_anomaly_duplicates, attributes below
+Required:
+    - built_in_rule_id
+    - display_name
+    - enabled
+    - log_analytics_workspace_id
+    - mode
+Optional:
+    - multi_select_observation (block):
+        - name (required)
+        - values (required)
+    - prioritized_exclude_observation (block):
+        - exclude (optional)
+        - name (required)
+        - prioritize (optional)
+    - single_select_observation (block):
+        - name (required)
+        - value (required)
+    - threshold_observation (block):
+        - name (required)
+        - value (required)
+EOT
+
+  type = map(object({
+    built_in_rule_id           = string
+    display_name               = string
+    enabled                    = bool
+    log_analytics_workspace_id = string
+    mode                       = string
+    multi_select_observation = optional(object({
+      name   = string
+      values = list(string)
+    }))
+    prioritized_exclude_observation = optional(object({
+      exclude    = optional(string)
+      name       = string
+      prioritize = optional(string)
+    }))
+    single_select_observation = optional(object({
+      name  = string
+      value = string
+    }))
+    threshold_observation = optional(object({
+      name  = string
+      value = string
+    }))
+  }))
+}
+
